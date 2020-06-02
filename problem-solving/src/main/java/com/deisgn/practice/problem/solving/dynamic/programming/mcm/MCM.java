1406f512ec2d3bc4cd20d59ef2b8ac2c92bdbab1
sidisl16 <sidisl16@gmail.com>
2020-06-02 10:34:10 +0530

    Dynamic programming
    Solved MCM and pallimdrome partitioning problem

---
+++ problem-solving/src/main/java/com/deisgn/practice/problem/solving/dynamic/programming/mcm/MCM.java
@@ -0,0 +1,49 @@
+package com.deisgn.practice.problem.solving.dynamic.programming.mcm;
+
+public class MCM {
+
+	public static int getMinCostRecursive(int a[], int i, int j) {
+
+		int min = Integer.MAX_VALUE;
+
+		if (i >= j) {
+			return 0;
+		}
+
+		for (int k = i; k <= j - 1; k++) {
+			int cost = getMinCostRecursive(a, i, k) + getMinCostRecursive(a, k + 1, j) + (a[i - 1] * a[k] * a[j]);
+			min = Math.min(min, cost);
+		}
+
+		return min;
+	}
+
+	private static int mem[][] = new int[5][5];
+
+	public static int getMinCostRecursiveMemoize(int a[], int i, int j) {
+
+		int min = Integer.MAX_VALUE;
+
+		if (i >= j) {
+			return 0;
+		}
+
+		if (mem[i][j] != 0) {
+			return mem[i][j];
+		}
+
+		for (int k = i; k <= j - 1; k++) {
+			int cost = getMinCostRecursive(a, i, k) + getMinCostRecursive(a, k + 1, j) + (a[i - 1] * a[k] * a[j]);
+			min = Math.min(min, cost);
+		}
+
+		return (mem[i][j] = min);
+	}
+
+	public static void main(String[] args) {
+
+		int a[] = { 40, 20, 30, 10, 30 };
+		System.out.println(getMinCostRecursiveMemoize(a, 1, a.length - 1));
+	}
+
+}
---
+++ problem-solving/src/main/java/com/deisgn/practice/problem/solving/dynamic/programming/mcm/PallimdromePartition.java
@@ -0,0 +1,81 @@
+package com.deisgn.practice.problem.solving.dynamic.programming.mcm;
+
+public class PallimdromePartition {
+
+	private static int mem[][] = new int[1000][1000];
+
+	// Recursive - bottom up approach
+	public static int getCost(String s, int i, int j) {
+
+		int min = Integer.MAX_VALUE;
+
+		if (i >= j) {
+			return 0;
+		}
+
+		if (mem[i][j] != 0) {
+			return mem[i][j];
+		}
+
+		if (isPallimdrome(s, i, j)) {
+			return 0;
+		}
+
+		for (int k = i; k < j; k++) {
+
+			int tempAns = getCost(s, i, k) + getCost(s, k + 1, j) + 1;
+			min = Math.min(min, tempAns);
+		}
+
+		return (mem[i][j] = min);
+	}
+
+	// Recursive - bottom up approach optimized
+	public static int getCostOptimised(String s, int i, int j) {
+
+		int min = Integer.MAX_VALUE;
+
+		if (i >= j) {
+			return 0;
+		}
+
+		if (mem[i][j] != 0) {
+			return mem[i][j];
+		}
+
+		if (isPallimdrome(s, i, j)) {
+			return 0;
+		}
+
+		for (int k = i; k < j; k++) {
+
+			int tempAns = (mem[i][k] != 0 ? mem[i][k] : getCost(s, i, k))
+					+ (mem[k + 1][j] != 0 ? mem[k + 1][j] : getCost(s, k + 1, j)) + 1;
+			min = Math.min(min, tempAns);
+		}
+
+		return (mem[i][j] = min);
+	}
+
+	private static boolean isPallimdrome(String s, int i, int j) {
+		while (i < j) {
+			if (s.charAt(i) != s.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		String s = "nitin";
+		System.out.println(getCostOptimised(s, 0, s.length() - 1));
+
+		mem = new int[1000][1000];
+
+		String s1 = "cabcbaa";
+		System.out.println(getCostOptimised(s1, 0, s1.length() - 1));
+	}
+
+}
